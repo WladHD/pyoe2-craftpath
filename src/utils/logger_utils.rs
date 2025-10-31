9ab85e09b1080c73b2ba89596d787f19b40d72cf
WladHD <wladhd@gmail.com>
2025-10-31 21:36:00 +0000

    feat(api): :children_crossing: use tracing to display console logs in Python too

--- src/utils/logger_utils.rs
+++ src/utils/logger_utils.rs
@@ -1,5 +1,3 @@
-#[cfg(not(feature = "python"))]
-#[allow(dead_code)] // somehow this gets marked as unused when running in cli mode .. although its called first... idk
 pub fn init_tracing() {
     static INIT: std::sync::Once = std::sync::Once::new();
     INIT.call_once(|| {
