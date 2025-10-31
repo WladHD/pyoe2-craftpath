092bd23d1a58d116437bc0fb6ce93d8c4564cdb8
WladHD <wladhd@gmail.com>
2025-10-31 21:37:42 +0000

    feat(api): :sparkles: offer `retrieve_jsons_from_urls_with_cache` as inbuilt Python method to load and cache web content

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 pub mod api;
 pub mod coe;
+pub mod external_api;
+pub mod pn;
 pub mod utils;
 
 #[cfg(feature = "python")]
@@ -9,9 +11,17 @@ pub mod py {
     use pyo3_stub_gen::define_stub_info_gatherer;
     use pyo3_stub_gen::derive::gen_stub_pyfunction;
 
+    use crate::api::currency::CraftCurrencyEnum;
     use crate::api::provider::item_info::ItemInfoProvider;
-    use crate::api::types::{AffixClassEnum, AffixDefinition, AffixId, AffixLocationEnum};
+    use crate::api::provider::market_prices::{
+        ItemName, MarketPriceProvider, PriceInDivines, PriceKind,
+    };
+    use crate::api::types::{
+        AffixClassEnum, AffixDefinition, AffixId, AffixLocationEnum, THashMap,
+    };
     use crate::coe::craftofexile_data_provider_adapter::CraftOfExileItemInfoProvider;
+    use crate::pn::poe_ninja_data_provider_adapter::PoeNinjaMarketPriceProvider;
+    use crate::utils::logger_utils::init_tracing;
 
     #[gen_stub_pyfunction]
     #[pyfunction]
@@ -20,13 +30,47 @@ pub mod py {
             .map_err(|err| PyRuntimeError::new_err(err.to_string()))
     }
 
+    #[gen_stub_pyfunction]
+    #[pyfunction]
+    fn parse_economy_from_jsons(json: Vec<String>) -> PyResult<MarketPriceProvider> {
+        PoeNinjaMarketPriceProvider::parse_from_json(json.as_slice())
+            .map_err(|err| PyRuntimeError::new_err(err.to_string()))
+    }
+
+    #[gen_stub_pyfunction]
+    #[pyfunction]
+    fn retrieve_jsons_from_urls_with_cache(
+        cache_url_map: THashMap<String, String>,
+        max_cache_duration_in_sec: u64,
+    ) -> PyResult<Vec<String>> {
+        crate::external_api::fetch_json_from_urls::retrieve_jsons_from_urls_with_cache(
+            cache_url_map,
+            max_cache_duration_in_sec,
+        )
+        .map_err(|err| PyRuntimeError::new_err(err.to_string()))
+    }
+
     #[pymodule]
     fn pyoe2_craftpath(m: &Bound<'_, PyModule>) -> PyResult<()> {
+        init_tracing();
+
         m.add_function(wrap_pyfunction!(parse_item_data_from_json, m)?)?;
         m.add_class::<AffixId>()?;
         m.add_class::<AffixDefinition>()?;
         m.add_class::<AffixClassEnum>()?;
         m.add_class::<AffixLocationEnum>()?;
+
+        m.add_function(wrap_pyfunction!(parse_economy_from_jsons, m)?)?;
+        m.add_class::<ItemName>()?;
+
+        m.add_class::<PriceInDivines>()?;
+        m.add_class::<PriceKind>()?;
+
+        m.add_class::<CraftCurrencyEnum>()?;
+
+        // general utility
+        m.add_function(wrap_pyfunction!(retrieve_jsons_from_urls_with_cache, m)?)?;
+
         Ok(())
     }
 
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 pub mod api;
 pub mod coe;
+pub mod external_api;
+pub mod pn;
 pub mod utils;
 
 fn main() {
