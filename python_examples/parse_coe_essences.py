a30255cd9c67ca588e1a5d193e1c891ecd5d3f6b
WladHD <wladhd@gmail.com>
2025-11-02 22:04:41 +0000

    docs(example): :sparkles: show achievable affix tier when essence is used

--- python_examples/parse_coe_essences.py
+++ python_examples/parse_coe_essences.py
@@ -16,11 +16,14 @@ def main():
         COE_CACHE_MAP, CACHE_TTL_IN_SECONDS)[0]
     data = pc.parse_item_data_from_json(text)
 
-    bow_base_item = pc.BaseItemId(20)
+    bow_base_item_id = pc.BaseItemId(20)
 
+    #####################
+    # TESTING ESSENCE ONLY AFFIX
+    #####################
     attack_speed_for_5697 = AffixId(5697)
     essence_id_for_5697 = data.lookup_affix_essences(
-        attack_speed_for_5697, bow_base_item)
+        attack_speed_for_5697, bow_base_item_id)
     print(essence_id_for_5697)
 
     base_mods_for_5697_20 = data.lookup_base_item_mods(pc.BaseItemId(20))
@@ -28,16 +31,41 @@ def main():
 
     assert possible_defs is not None
 
-    for i, (k, v) in enumerate(possible_defs.items()):
-        print("#", i + 1, " ", k, " : ", v)
-
     assert essence_id_for_5697.__contains__(
         # Greater Essence of Haste
         pc.EssenceId(3156))
 
+    for essence in essence_id_for_5697:
+        definition = data.lookup_essence_definition(essence)
+
+        print("Printing definition for essence: ", definition.name_essence)
+
+        possible_defs = definition.base_tier_table.get(pc.BaseItemId(20))
+
+        assert possible_defs is not None
+
+        for i, (k, v) in enumerate(possible_defs.items()):
+            print("#", i + 1, " ", k, " : ", v)
+
+            defi = data.lookup_base_item_mods(bow_base_item_id)
+
+            info = defi.get(attack_speed_for_5697)
+
+            assert info is not None
+
+            equivalent_affix_tier = next(((k2, v2) for k2, v2 in info.items(
+            ) if v2.min_item_level == v.min_item_level), None)
+
+            assert equivalent_affix_tier is not None
+
+            print("Will achieve tier: ", equivalent_affix_tier[0])
+
+    #####################
+    # TESTING NORMAL AFFIX; THAT CAN BE REACHED WITH AN ESSENCE
+    #####################
     attack_speed = AffixId(5092)
     essence_id_for_5092 = data.lookup_affix_essences(
-        attack_speed, bow_base_item)
+        attack_speed, bow_base_item_id)
 
     # should exactly be 2 (lesser + normal essence of haste)
     assert essence_id_for_5092.__len__() == 2
@@ -64,8 +92,24 @@ def main():
         for i, (k, v) in enumerate(possible_defs.items()):
             print("#", i + 1, " ", k, " : ", v)
 
+            defi = data.lookup_base_item_mods(bow_base_item_id)
+
+            info = defi.get(attack_speed)
+
+            assert info is not None
+
+            equivalent_affix_tier = next(((k2, v2) for k2, v2 in info.items(
+            ) if v2.min_item_level == v.min_item_level), None)
+
+            assert equivalent_affix_tier is not None
+
+            print("Will achieve tier: ", equivalent_affix_tier[0])
+
     # Greater Essence of Haste -> has own essence-only affix
     essence = data.lookup_essence_definition(pc.EssenceId(3156))
+
+    assert essence is not None
+
     print(essence)
 
     # If this test finishes without errors, it works as intended
