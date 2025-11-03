5e2cb605a9f7bca2b3fc01770c729f368c3b3c0e
WladHD <wladhd@gmail.com>
2025-11-03 22:42:49 +0000

    feat(api): :children_crossing: use Rust debug strings for Python `print` if serialization fails

--- src/utils/debug_display_macro.rs
+++ src/utils/debug_display_macro.rs
@@ -6,7 +6,7 @@ macro_rules! derive_DebugDisplay {
                 fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
                     match serde_json::to_string_pretty(self) {
                         Ok(json) => write!(f, "{}", json),
-                        Err(e) => write!(f, "<failed to serialize {}: {}>", stringify!($t), e),
+                        Err(e) => write!(f, "<failed to serialize {}: {}>\nPrinting raw Rust debug string: {:#?}", stringify!($t), e, self),
                     }
                 }
             }
